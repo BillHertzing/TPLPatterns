1bfa8c61c008ce710e33b9b7560a96769efcd058
Bill Hertzing <bill.hertzing@gmail.com>
2017-11-20 09:31:19 -0500

    Initial implementation of ObservableConcurrentDictionary and some basic working tests

--- TPLPatterns/TPLPatterns.cs
+++ TPLPatterns/TPLPatterns.cs
@@ -28,4 +28,36 @@ namespace TPLPatternExamples
             calculatedResults = new ConcurrentDictionary<string, ConcurrentDictionary<string, decimal>>();
         }
     }
+    public class WithObservableConcurrentDictionary
+    {
+        public ObservableConcurrentDictionary<string, ObservableConcurrentDictionary<string, decimal>>  calculatedResults;
+
+
+        public void RecordCalculatedResults(string k1, string k2, decimal pr)
+        {
+            if (calculatedResults.ContainsKey(k1))
+            {
+                var innerDictionary = calculatedResults[k1];
+                if (innerDictionary.ContainsKey(k2))
+                {
+                    throw new NotSupportedException("This pattern expects only one entry per k1k2 pair");
+                }
+                else
+                {
+                    //ToDo: Better understanding/handling of exceptions here
+                    try { innerDictionary.Add(k2, pr); } catch { new Exception($"adding {pr} to {k1}'s innerDictionary keyed by {k2} failed"); }
+                }
+            }
+            else
+            {
+                var innerDictionary = new ObservableConcurrentDictionary<string, decimal>();
+                try { innerDictionary.Add(k2, pr); } catch { new Exception($"adding {pr} to the new innerDictionary keyed by {k2} failed"); }
+                try { calculatedResults.Add(k1, innerDictionary); } catch { new Exception($"adding the new innerDictionary to calculatedResults keyed by {k1} failed"); }
+            };
+        }
+        public WithObservableConcurrentDictionary()
+        {
+            calculatedResults = new ObservableConcurrentDictionary<string, ObservableConcurrentDictionary<string, decimal>>();
+        }
+    }
 }
--- TPLPatternsUnitTests/CRDictionaryBasicTests.cs
+++ TPLPatternsUnitTests/CRDictionaryBasicTests.cs
@@ -23,12 +23,12 @@ namespace TPLPatternsUnitTests
             tplPatterns.RecordCalculatedResults(k1, k2, pr);
             string fs = "";
             tplPatterns.calculatedResults.SelectMany(
-                pair => pair.Value.Select(
+                pair => pair.Value.SelectMany(
                     innerPair => $"{pair.Key},{innerPair.Key},{innerPair.Value};")).ToList().ForEach(x => fs += x.ToString());
             Assert.Equal("k1=1,k2=2,1.1;", fs);
         }
 
-        // Some of the InlineData is not sorted, but the query over the dictionaries will sort, so the asserts are for teh results in sorted order
+        // Some of the InlineData is not sorted, but the query over the dictionaries will sort, so the asserts are for the results in sorted order
         [Theory, InlineData( "k1=2,k2=2,2.2;k1=2,k2=1,2.1;k1=1,k2=1,1.1;k1=1,k2=2,1.2;")]
         public void ValidateCRFlattened2And2(string str)
         {
@@ -47,5 +47,34 @@ namespace TPLPatternsUnitTests
                     .ToList().ForEach(x => fs += x.ToString());
             Assert.Equal("k1=1,k2=1,1.1;k1=1,k2=2,1.2;k1=2,k2=1,2.1;k1=2,k2=2,2.2;", fs);
         }
+    }
+    public class ObservableConcurrentDictionaryForResultsBasicTests
+    {
+        [Theory, InlineData("k1=1", "k2=2", 1.0)]
+        public void ValidateOCRHasOneCount(string k1, string k2, decimal pr)
+        {
+            WithObservableConcurrentDictionary withObservableConcurrentDictionary = new WithObservableConcurrentDictionary();
+            withObservableConcurrentDictionary.RecordCalculatedResults(k1, k2, pr);
+            Assert.Equal(1, withObservableConcurrentDictionary.calculatedResults.Count());
+        }
+        // Some of the InlineData is not sorted, but the query over the dictionaries will sort, so the asserts are for the results in sorted order
+        [Theory, InlineData("k1=2,k2=2,2.2;k1=2,k2=1,2.1;k1=1,k2=1,1.1;k1=1,k2=2,1.2;")]
+        public void ValidateOOCRFlattened2And2(string str)
+        {
+            WithObservableConcurrentDictionary withObservableConcurrentDictionary = new WithObservableConcurrentDictionary();
+            var match = new Regex("(?<k1>.*?),(?<k2>.*?),(?<pr>.*?);").Match(str);
+            while (match.Success)
+            {
+                withObservableConcurrentDictionary.RecordCalculatedResults(match.Groups["k1"].Value, match.Groups["k2"].Value, decimal.Parse(match.Groups["pr"].Value));
+                match = match.NextMatch();
+            }
+            string fs = "";
+            withObservableConcurrentDictionary.calculatedResults.OrderBy(kp => kp.Key).SelectMany(
+                pair => pair.Value.OrderBy(kp => kp.Key).SelectMany(
+                    innerPair => $"{pair.Key},{innerPair.Key},{innerPair.Value};"))
+                    .ToList().ForEach(x => fs += x.ToString());
+            Assert.Equal("k1=1,k2=1,1.1;k1=1,k2=2,1.2;k1=2,k2=1,2.1;k1=2,k2=2,2.2;", fs);
+        }
+
     }
 }
