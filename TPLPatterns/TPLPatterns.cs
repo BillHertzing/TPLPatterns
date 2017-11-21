36a7076f21d289d45402aa6d77b3df66a8b0a813
Bill Hertzing <bill.hertzing@gmail.com>
2017-11-20 23:32:00 -0500

    Intial code and tests for an observableconcurrendictionary to hold the results

--- TPLPatterns/TPLPatterns.cs
+++ TPLPatterns/TPLPatterns.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Swordfish.NET.Collections;
+using System;
 using System.Collections.Concurrent;
+using System.Collections.Specialized;
+using System.ComponentModel;
 
 namespace TPLPatternExamples
 {
@@ -30,7 +33,7 @@ namespace TPLPatternExamples
     }
     public class WithObservableConcurrentDictionary
     {
-        public ObservableConcurrentDictionary<string, ObservableConcurrentDictionary<string, decimal>>  calculatedResults;
+        public ConcurrentObservableDictionary<string, ConcurrentObservableDictionary<string, decimal>>  calculatedResults;
 
 
         public void RecordCalculatedResults(string k1, string k2, decimal pr)
@@ -50,14 +53,93 @@ namespace TPLPatternExamples
             }
             else
             {
-                var innerDictionary = new ObservableConcurrentDictionary<string, decimal>();
+                var innerDictionary = new ConcurrentObservableDictionary<string, decimal>();
                 try { innerDictionary.Add(k2, pr); } catch { new Exception($"adding {pr} to the new innerDictionary keyed by {k2} failed"); }
                 try { calculatedResults.Add(k1, innerDictionary); } catch { new Exception($"adding the new innerDictionary to calculatedResults keyed by {k1} failed"); }
             };
         }
         public WithObservableConcurrentDictionary()
         {
-            calculatedResults = new ObservableConcurrentDictionary<string, ObservableConcurrentDictionary<string, decimal>>();
+            calculatedResults = new ConcurrentObservableDictionary<string, ConcurrentObservableDictionary<string, decimal>>();
         }
+    }
+
+    public class WithObservableConcurrentDictionaryAndEventHandlers : IDisposable
+    {
+        public ConcurrentObservableDictionary<string, ConcurrentObservableDictionary<string, decimal>> calculatedResults;
+
+        NotifyCollectionChangedEventHandler onCollectionChanged;
+        PropertyChangedEventHandler onPropertyChanged;
+        public void RecordCalculatedResults(string k1, string k2, decimal pr)
+        {
+            if (calculatedResults.ContainsKey(k1))
+            {
+                var innerDictionary = calculatedResults[k1];
+                if (innerDictionary.ContainsKey(k2))
+                {
+                    throw new NotSupportedException("This pattern expects only one entry per k1k2 pair");
+                }
+                else
+                {
+                    //ToDo: Better understanding/handling of exceptions here
+                    try { innerDictionary.Add(k2, pr); } catch { new Exception($"adding {pr} to {k1}'s innerDictionary keyed by {k2} failed"); }
+                }
+            }
+            else
+            {
+                var innerDictionary = new ConcurrentObservableDictionary<string, decimal>();
+                try { innerDictionary.Add(k2, pr); } catch { new Exception($"adding {pr} to the new innerDictionary keyed by {k2} failed"); }
+                try { calculatedResults.Add(k1, innerDictionary); } catch { new Exception($"adding the new innerDictionary to calculatedResults keyed by {k1} failed"); }
+            };
+        }
+        public WithObservableConcurrentDictionaryAndEventHandlers(NotifyCollectionChangedEventHandler OnCollectionChanged, PropertyChangedEventHandler OnPropertyChanged)
+        {
+            calculatedResults = new ConcurrentObservableDictionary<string, ConcurrentObservableDictionary<string, decimal>>();
+            this.onCollectionChanged = OnCollectionChanged;
+            this.onPropertyChanged = OnPropertyChanged;
+            calculatedResults.CollectionChanged += this.onCollectionChanged;
+            calculatedResults.PropertyChanged += this.onPropertyChanged;
+        }
+
+        public void TearDown() { 
+            calculatedResults.CollectionChanged -= this.onCollectionChanged;
+            calculatedResults.PropertyChanged -= this.onPropertyChanged;
+}
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // dispose managed state (managed objects).
+                    TearDown();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~WithObservableConcurrentDictionaryAndEventHandlers() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
--- TPLPatternsUnitTests/CRDictionaryBasicTests.cs
+++ TPLPatternsUnitTests/CRDictionaryBasicTests.cs
@@ -1,80 +1,177 @@
 using System;
-using Xunit;
-using TPLPatternExamples;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TPLPatternExamples;
+using Xunit;
+using Xunit.Abstractions;
 
-namespace TPLPatternsUnitTests
-{
-    public class CRDictionaryBasicTests
-    {
-        [Theory, InlineData("k1=1","k2=2",1.0)]
-        public void ValidateCRHasOneCount(string k1,string k2, decimal pr)
-        {
+namespace TPLPatternsUnitTests {
+    public class ConcurrentDictionaryForResultsBasicTests {
+        [Theory]
+        [InlineData("k1=1", "k2=2", 1.1)]
+        public void CRFlattened1And1(string k1, string k2, decimal pr) {
             TPLPatterns tplPatterns = new TPLPatterns();
             tplPatterns.RecordCalculatedResults(k1, k2, pr);
-            Assert.Equal(1, tplPatterns.calculatedResults.Count);
-        }
-        [Theory, InlineData("k1=1", "k2=2", 1.1)]
-        public void ValidateCRFlattened1And1(string k1, string k2, decimal pr)
-        {
-            TPLPatterns tplPatterns = new TPLPatterns();
-            tplPatterns.RecordCalculatedResults(k1, k2, pr);
-            string fs = "";
-            tplPatterns.calculatedResults.SelectMany(
-                pair => pair.Value.SelectMany(
-                    innerPair => $"{pair.Key},{innerPair.Key},{innerPair.Value};")).ToList().ForEach(x => fs += x.ToString());
+            string fs = string.Empty;
+            tplPatterns.calculatedResults.SelectMany(pair => pair.Value.SelectMany(innerPair => $"{pair.Key},{innerPair.Key},{innerPair.Value};"))
+                .ToList()
+                .ForEach(x => fs += x.ToString());
             Assert.Equal("k1=1,k2=2,1.1;", fs);
         }
 
         // Some of the InlineData is not sorted, but the query over the dictionaries will sort, so the asserts are for the results in sorted order
-        [Theory, InlineData( "k1=2,k2=2,2.2;k1=2,k2=1,2.1;k1=1,k2=1,1.1;k1=1,k2=2,1.2;")]
-        public void ValidateCRFlattened2And2(string str)
-        {
+        [Theory]
+        [InlineData("k1=2,k2=2,2.2;k1=2,k2=1,2.1;k1=1,k2=1,1.1;k1=1,k2=2,1.2;")]
+        public void CRFlattened2And2(string str) {
             TPLPatterns tplPatterns = new TPLPatterns();
             Regex RE = new Regex("(?<k1>.*?),(?<k2>.*?),(?<pr>.*?);");
             var match = RE.Match(str);
-            while (match.Success)
-            {
-                tplPatterns.RecordCalculatedResults(match.Groups["k1"].Value, match.Groups["k2"].Value, decimal.Parse(match.Groups["pr"].Value));
+            while(match.Success) {
+                tplPatterns.RecordCalculatedResults(match.Groups["k1"].Value,
+                                                    match.Groups["k2"].Value,
+                                                    decimal.Parse(match.Groups["pr"].Value));
                 match = match.NextMatch();
             }
-            string fs = "";
-            tplPatterns.calculatedResults.OrderBy(kp=>kp.Key).SelectMany(
-                pair => pair.Value.OrderBy(kp => kp.Key).SelectMany(
-                    innerPair => $"{pair.Key},{innerPair.Key},{innerPair.Value};"))
-                    .ToList().ForEach(x => fs += x.ToString());
-            Assert.Equal("k1=1,k2=1,1.1;k1=1,k2=2,1.2;k1=2,k2=1,2.1;k1=2,k2=2,2.2;", fs);
-        }
-    }
-    public class ObservableConcurrentDictionaryForResultsBasicTests
-    {
-        [Theory, InlineData("k1=1", "k2=2", 1.0)]
-        public void ValidateOCRHasOneCount(string k1, string k2, decimal pr)
-        {
-            WithObservableConcurrentDictionary withObservableConcurrentDictionary = new WithObservableConcurrentDictionary();
-            withObservableConcurrentDictionary.RecordCalculatedResults(k1, k2, pr);
-            Assert.Equal(1, withObservableConcurrentDictionary.calculatedResults.Count());
-        }
-        // Some of the InlineData is not sorted, but the query over the dictionaries will sort, so the asserts are for the results in sorted order
-        [Theory, InlineData("k1=2,k2=2,2.2;k1=2,k2=1,2.1;k1=1,k2=1,1.1;k1=1,k2=2,1.2;")]
-        public void ValidateOOCRFlattened2And2(string str)
-        {
-            WithObservableConcurrentDictionary withObservableConcurrentDictionary = new WithObservableConcurrentDictionary();
-            var match = new Regex("(?<k1>.*?),(?<k2>.*?),(?<pr>.*?);").Match(str);
-            while (match.Success)
-            {
-                withObservableConcurrentDictionary.RecordCalculatedResults(match.Groups["k1"].Value, match.Groups["k2"].Value, decimal.Parse(match.Groups["pr"].Value));
-                match = match.NextMatch();
-            }
-            string fs = "";
-            withObservableConcurrentDictionary.calculatedResults.OrderBy(kp => kp.Key).SelectMany(
-                pair => pair.Value.OrderBy(kp => kp.Key).SelectMany(
-                    innerPair => $"{pair.Key},{innerPair.Key},{innerPair.Value};"))
-                    .ToList().ForEach(x => fs += x.ToString());
-            Assert.Equal("k1=1,k2=1,1.1;k1=1,k2=2,1.2;k1=2,k2=1,2.1;k1=2,k2=2,2.2;", fs);
+            string fs = string.Empty;
+            tplPatterns.calculatedResults.OrderBy(kp => kp.Key)
+                .SelectMany(pair => pair.Value.OrderBy(kp => kp.Key)
+                                        .SelectMany(innerPair => $"{pair.Key},{innerPair.Key},{innerPair.Value};"))
+                .ToList()
+                .ForEach(x => fs += x.ToString());
+            Assert.Equal("k1=1,k2=1,1.1;k1=1,k2=2,1.2;k1=2,k2=1,2.1;k1=2,k2=2,2.2;",
+                         fs);
         }
 
+        [Theory]
+        [InlineData("k1=1", "k2=2", 1.0)]
+        public void ValidateCRHasOneCount(string k1, string k2, decimal pr) {
+            TPLPatterns tplPatterns = new TPLPatterns();
+            tplPatterns.RecordCalculatedResults(k1, k2, pr);
+            Assert.Equal(1, tplPatterns.calculatedResults.Count);
+        }
+    }
+
+    public class ObservableConcurrentDictionaryForResultsBasicTests {
+        readonly ITestOutputHelper output;
+
+        public ObservableConcurrentDictionaryForResultsBasicTests(ITestOutputHelper output) {
+            this.output = output;
+        }
+
+        // Use an observer class to watch the events on the ObservableConcurrentDictionary
+        [Theory]
+        [InlineData("k1=1,k2=1,1.1;")]
+        public void OCREventsViaObserver(string str) {
+            WithObservableConcurrentDictionary withObservableConcurrentDictionary = new WithObservableConcurrentDictionary();
+            void RecordResults(string instr)
+            {
+                var match = new Regex("(?<k1>.*?),(?<k2>.*?),(?<pr>.*?);").Match(instr);
+                while(match.Success) {
+                    withObservableConcurrentDictionary.RecordCalculatedResults(match.Groups["k1"].Value,
+                                                                               match.Groups["k2"].Value,
+                                                                               decimal.Parse(match.Groups["pr"].Value));
+                    match = match.NextMatch();
+                }
+            }
+
+            List<string> receivedEvents = new List<string>();
+
+            // attach a handler to the PropertyChanged event of the outer dictionary
+            withObservableConcurrentDictionary.calculatedResults.PropertyChanged += delegate(object sender, PropertyChangedEventArgs e) { receivedEvents.Add(e.PropertyName); };
+            // attach a handler to the CollectionChanged event of the outer dictionary
+            withObservableConcurrentDictionary.calculatedResults.CollectionChanged += delegate(object sender, NotifyCollectionChangedEventArgs e) { receivedEvents.Add($"{e.Action} NumItemsToAdd {e.NewItems.Count}"); };
+            // populate the OCR
+            RecordResults(str);
+            Task.Delay(1000);
+            //output.WriteLine($"{receivedEvents.Count}");
+            receivedEvents.ForEach(x => output.WriteLine($"{x}"));
+            //Assert.Equal(2, receivedEvents.Count);
+            //Assert.Equal("CollectionView", receivedEvents[0]);
+            //Assert.Equal("Count", receivedEvents[1]);
+            Assert.Equal(2, 2);
+        }
+
+        // Some of the InlineData is not sorted, but the query over the dictionaries will sort, so the asserts are for the results in sorted order
+        [Theory]
+        [InlineData("k1=2,k2=2,2.2;k1=2,k2=1,2.1;k1=1,k2=1,1.1;k1=1,k2=2,1.2;")]
+        public void OCRFlattened2And2(string str) {
+            WithObservableConcurrentDictionary withObservableConcurrentDictionary = new WithObservableConcurrentDictionary();
+            var match = new Regex("(?<k1>.*?),(?<k2>.*?),(?<pr>.*?);").Match(str);
+            while(match.Success) {
+                withObservableConcurrentDictionary.RecordCalculatedResults(match.Groups["k1"].Value,
+                                                                           match.Groups["k2"].Value,
+                                                                           decimal.Parse(match.Groups["pr"].Value));
+                match = match.NextMatch();
+            }
+            string fs = string.Empty;
+            withObservableConcurrentDictionary.calculatedResults.OrderBy(kp => kp.Key)
+                .SelectMany(pair => pair.Value.OrderBy(kp => kp.Key)
+                                        .SelectMany(innerPair => $"{pair.Key},{innerPair.Key},{innerPair.Value};"))
+                .ToList()
+                .ForEach(x => fs += x.ToString());
+            Assert.Equal("k1=1,k2=1,1.1;k1=1,k2=2,1.2;k1=2,k2=1,2.1;k1=2,k2=2,2.2;",
+                         fs);
+        }
+
+        [Theory]
+        [InlineData("k1=1", "k2=2", 1.0)]
+        public void OCRHasOneCount(string k1, string k2, decimal pr) {
+            WithObservableConcurrentDictionary withObservableConcurrentDictionary = new WithObservableConcurrentDictionary();
+            withObservableConcurrentDictionary.RecordCalculatedResults(k1,
+                                                                       k2,
+                                                                       pr);
+            Assert.Equal(1,
+                         withObservableConcurrentDictionary.calculatedResults.Count());
+        }
+
+        
+
+        // Test via an observer that the Collection changed event gets raised
+        [Theory]
+        [InlineData("k1=1,k2=1,1.1;")]
+        [InlineData("k1=2,k2=2,2.2;k1=2,k2=1,2.1;k1=1,k2=1,1.1;k1=1,k2=2,1.2;")]
+        public void OCRPropertyChangedEventsViaObserver(string str)
+        {
+            // Going to use a ConcurrentDictionary to hold the information written by teh event handlers
+            ConcurrentDictionary<string,long> receivedEvents = new ConcurrentDictionary<string, long>();
+
+            // This event handler will be attached/detached from the ObservableConcurrentDictionary via that class' constructor and dispose method
+            void onNotifyCollectionChanged(object sender,
+    NotifyCollectionChangedEventArgs e)
+            {
+                receivedEvents[$"Time: {DateTime.Now} Event: NotifyCollectionChanged Action:{e.Action} NumItemsToAdd {e.NewItems.Count}"]=  DateTime.Now.Ticks;
+            };
+            void onPropertyChanged(object sender, PropertyChangedEventArgs e)
+            {
+                receivedEvents[$"Time: {DateTime.Now} Event: PropertyChanged PropertyName {e.PropertyName}"] = DateTime.Now.Ticks;
+
+                            };
+            WithObservableConcurrentDictionaryAndEventHandlers withObservableConcurrentDictionaryAndEventHandlers = new WithObservableConcurrentDictionaryAndEventHandlers(onNotifyCollectionChanged, onPropertyChanged);
+            void RecordResults(string instr)
+            {
+                var match = new Regex("(?<k1>.*?),(?<k2>.*?),(?<pr>.*?);").Match(instr);
+                while (match.Success)
+                {
+                    withObservableConcurrentDictionaryAndEventHandlers.RecordCalculatedResults(match.Groups["k1"].Value,
+                                                                               match.Groups["k2"].Value,
+                                                                               decimal.Parse(match.Groups["pr"].Value));
+                    match = match.NextMatch();
+                }
+            }
+
+            // populate the OCR
+            RecordResults(str);
+            Task.Delay(1000);
+            receivedEvents.Keys.OrderBy(x => receivedEvents[x]).ToList().ForEach(x => output.WriteLine($"{x} : {receivedEvents[x]}"));
+            //Assert.Equal(2, receivedEvents.Count);
+            //Assert.Equal("CollectionView", receivedEvents[0]);
+            //Assert.Equal("Count", receivedEvents[1]);
+            Assert.Equal(2, 2);
+        }
     }
 }
